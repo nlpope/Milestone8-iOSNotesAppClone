f1d252215f7bdd4e1896eb516bf4aeb3d5e433c7
Noah Pope <npope@tutanota.com>
2025-05-03 14:00:55 -0500

    adding 'add folder' button + creating dictionary for sections & underlying cells

--- Milestone8-iOSNotesAppClone/Screens/HomeFoldersVC.swift
+++ Milestone8-iOSNotesAppClone/Screens/HomeFoldersVC.swift
@@ -7,11 +7,22 @@ import UIKit
 class HomeFoldersVC: UITableViewController
 {
     @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var addFolderBtn: UITabBarItem!
+    var sectionDictionary: Dictionary = [
+        "": ["Quick Notes", "Shared"],
+        "iCloud": ["All iCloud", "Notes"],
+        "On My iPhone": ["Notes"]
+    ]
+    var sectionDictKeys: Dictionary<String,[String]>.Keys!
+    var sectionDictVals: Dictionary<String,[String]>.Values!
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setNavigation()
+        sectionDictKeys = sectionDictionary.keys
+        sectionDictVals = sectionDictionary.values
     }
     
     //-------------------------------------//
@@ -21,14 +32,28 @@ class HomeFoldersVC: UITableViewController
     {
         view.backgroundColor    = .systemBackground
         title                   = "Folders"
+    }
+    
+    //-------------------------------------//
+    // MARK: TABLEVC DELEGATE & DATASOURCE METHODS
+    
+    override func numberOfSections(in tableView: UITableView) -> Int { return sectionDictKeys.count }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return sectionDictVals.count }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if cell == nil { cell = UITableViewCell(style: .default, reuseIdentifier: "Cell") }
         
+        cell.textLabel?.text    = "testing"
+        
+        return cell
     }
     
     
-    override func numberOfSections(in tableView: UITableView) -> Int { return 3 }
-    
-    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        <#code#>
+        return sectionDictKeys[section]
     }
 }
