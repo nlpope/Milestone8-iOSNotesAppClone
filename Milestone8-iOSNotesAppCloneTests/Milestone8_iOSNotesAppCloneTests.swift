a210f4969fd47911909fedb5ccbc73274ee7cc9e
Noah Pope <npope@tutanota.com>
2025-05-03 02:32:21 -0500

    Initial Commit

---
+++ Milestone8-iOSNotesAppCloneTests/Milestone8_iOSNotesAppCloneTests.swift
@@ -0,0 +1,14 @@
+//  File: Milestone8_iOSNotesAppCloneTests.swift
+//  Project: Milestone8-iOSNotesAppCloneTests
+//  Created by: Noah Pope on 5/3/25.
+
+import Testing
+@testable import Milestone8_iOSNotesAppClone
+
+struct Milestone8_iOSNotesAppCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
